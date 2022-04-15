80f98cfe7b9eb716b1faf67ab06949c8438b3933
Devendra <devendraaele@gmail.com>
2022-04-15 18:24:36 +0530

    Create Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './componant/Header';
+import Contant from './componant/Contant';
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Contant/>
+   </>
+  );
+}
+
+export default App;
